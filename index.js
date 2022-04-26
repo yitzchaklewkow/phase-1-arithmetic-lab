e279395fad18ed0e6fc7facfab512dc4e3441c3b
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-04-25 19:20:57 -0600

    commit tree a797b1d18bf26ed3269365cc5d40b0fd4880e0f6

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-
-// Write your code here
+const num1 = 31, num2 = 2, num3 = 9, num4 = 5;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random()) + 1;
+const mod = num3 % num4;
+const max = Math.max(4,18,4,2.5,20,3);
